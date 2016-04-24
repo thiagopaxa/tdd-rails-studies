c41fc93ef9426968c71b0503f2bbf0fbd32b4a9d
Thiago Paixão <tpaxadev@gmail.com>
2016-04-24 01:36:11 -0300

    making presence validations some other way

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -6,4 +6,31 @@ RSpec.describe Band, type: :model do
   it { is_expected.to validate_presence_of(:site) }
 
   it { is_expected.to have_many(:musics) }
+
+  context "is valid when" do
+    it 'has a valid factory' do
+      expect(build(:band)).to be_valid
+    end
+  end
+
+  context "is invalid when" do
+    it 'has no name' do
+      band = build(:band, name: nil)
+      band.valid?
+      expect(band.errors[:name]).to include("can't be blank")
+    end
+
+    it 'has no musical_genre' do
+      band = build(:band, musical_genre: nil)
+      band.valid?
+      expect(band.errors[:musical_genre]).to include("can't be blank")
+    end
+
+    it 'has no site' do
+      band = build(:band, site: nil)
+      band.valid?
+      expect(band.errors[:site]).to include("can't be blank")
+    end
+  end
+
 end
